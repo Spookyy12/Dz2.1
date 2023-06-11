8a5aafa6e5069185af43b6b66078cfb7e3b21e12
Spookyy12 <131680995+Spookyy12@users.noreply.github.com>
2023-06-11 23:20:36 +0300

    firstCom

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = 100;
+        int bonus = 0;
+        int repl = 200;
+
+        if (repl > 999) {
+            bonus = repl / 100;
+        } else {
+            bonus = 0;
+        }
+        balance = balance + repl + bonus;
+        System.out.println(bonus);
+        System.out.println(balance);
+
+    }
+}
